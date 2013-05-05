76346b7086b0a4432bbb7ae49a9c384e98a612e3
Zainab 3brahimi <zainabebrahimi@gmail.com>
2013-05-05 11:33:39 -0400

    prettified - a lil

--- salmon.js
+++ salmon.js
@@ -1,50 +1,58 @@
-   function calculateRoute(from, to) {
-        // Center initialized to Naples, Italy
-        var myOptions = {
-          zoom: 10,
-          center: new google.maps.LatLng(40.7142, 74.0064),
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        // Draw the map
-        var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-        var directionsService = new google.maps.DirectionsService();
-        var directionsRequest = {
-          origin: from,
-          destination: to,
-          travelMode: google.maps.DirectionsTravelMode.WALKING,
-          provideRouteAlternatives: true,
-          unitSystem: google.maps.UnitSystem.METRIC
-        };
-        directionsService.route(
-          directionsRequest,
-          function(response, status)
-          {
-            if (status == google.maps.DirectionsStatus.OK)
-            {
-              var display = new google.maps.DirectionsRenderer({
+/*
+* Big Apps NYC hackathon - healthy living
+* 
+* by zeeberry + alonecuzzo 
+*
+* Many thanks to Aurelio De Rosa
+* http://www.sitepoint.com/find-a-route-using-the-geolocation-and-the-google-maps-api/
+*
+*/
+
+function calculateRoute(from, to) {
+// Center initialized to New York
+  var myOptions = {
+      zoom: 10,
+      center: new google.maps.LatLng(40.7142, 74.0064),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  // Draw the map
+  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+  var directionsService = new google.maps.DirectionsService();
+  var directionsRequest = {
+        origin: from,
+        destination: to,
+        travelMode: google.maps.DirectionsTravelMode.TRANSIT,
+        provideRouteAlternatives: true,
+        unitSystem: google.maps.UnitSystem.METRIC
+      };
+      
+  directionsService.route(directionsRequest,function(response, status){
+      if (status == google.maps.DirectionsStatus.OK){
+          var display = new google.maps.DirectionsRenderer({
                 map: mapObject,
                 directions: response
-              });
+            });
+          display.setPanel(document.getElementById("directions"));
+      }else
+          $("#error").append("Unable to retrieve your route<br />");
+    });
+}
 
-              display.setPanel(document.getElementById("directions"));
-            }
-            else
-              $("#error").append("Unable to retrieve your route<br />");
-          }
-        );
-      }
-      $(document).ready(function() {
-        // If the browser supports the Geolocation API
-        if (typeof navigator.geolocation == "undefined") {
-          $("#error").text("Your browser doesn't support the Geolocation API");
-          return;
-        }
-        $("#from-link, #to-link").click(function(event) {
-          event.preventDefault();
-          var addressId = this.id.substring(0, this.id.indexOf("-"));
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var geocoder = new google.maps.Geocoder();
-            geocoder.geocode({
+$(document).ready(function() {
+// If the browser supports the Geolocation API
+  if (typeof navigator.geolocation == "undefined") {
+
+      $("#error").text("Your browser doesn't support the Geolocation API");
+
+      return;
+  }
+  //Getting geolocation
+  $("#from-link, #to-link").click(function(event) {
+      event.preventDefault();
+      var addressId = this.id.substring(0, this.id.indexOf("-"));
+      navigator.geolocation.getCurrentPosition(function(position) {
+          var geocoder = new google.maps.Geocoder();
+          geocoder.geocode({
               "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
             },
             function(results, status) {
@@ -61,19 +69,10 @@
             enableHighAccuracy: true,
             timeout: 10 * 1000 // 10 seconds
           });
-        });
-        $("#calculate-route").submit(function(event) {
+  });
+  
+  $("#calculate-route").submit(function(event) {
           event.preventDefault();
           calculateRoute($("#from").val(), $("#to").val());
         });
       });
-// function initialize() {
-//   var mapOptions = {
-//       center: new google.maps.LatLng(-34.397, 150.644),
-//       zoom: 8,
-//       mapTypeId: google.maps.MapTypeId.ROADMAP
-//    };
-//   var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-// }
-
-// google.maps.event.addDomListener(window, 'load', initialize);
